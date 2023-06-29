dbb588ec6e30f9b8347d6fa051f6a5d9a66c1ecf
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-29 15:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let op = document.getElementById("output");
+function Prominse(resolve,reject) {
+	resolve("Hello, world!");
+}
+let prom1 = new Promise();
+
+prom1.then(data)=>{
+	op.innerText = data;
+}
